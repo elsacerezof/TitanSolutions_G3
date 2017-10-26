ae384b5151e2441d691e29cfde2a0dc5c494fac5
Javier <jmv52@alumnos.unican.es>
2017-10-26 09:45:53 +0200

    Correcciones menores en línea base

--- Src/ProyectoTUS/app/src/main/java/es/unican/elsacf/proyectotus/ParadasFragment.java
+++ Src/ProyectoTUS/app/src/main/java/es/unican/elsacf/proyectotus/ParadasFragment.java
@@ -18,35 +18,33 @@ import android.view.ViewGroup;
  * create an instance of this fragment.
  */
 public class ParadasFragment extends Fragment {
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
+    // TODO: Declarar los argumentos de los parametros de inicializacion
+    // parametros de inicializacion del fragmento
+    //private static final String ARG_PARAM1 = "param1";
+    //private static final String ARG_PARAM2 = "param2";
 
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
+    // TODO: Declarar parametros
+    //private String mParam1;
+    //private String mParam2;
 
     private OnFragmentInteractionListener mListener;
 
     public ParadasFragment() {
-        // Required empty public constructor
+        // Requerido un constructor publico vacio
     }
 
     /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
+     * Utilizar este metodo para crear una nueva instancia
+     * de este fragmento utilizando los parametros proporcionadoste
      *
      * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
      * @return A new instance of fragment ParadasFragment.
      */
-    // TODO: Rename and change types and number of parameters
-    public static ParadasFragment newInstance(String param1, String param2) {
+    // TODO: cambiar los parametros que procedan
+    public static ParadasFragment newInstance(String param1) {
         ParadasFragment fragment = new ParadasFragment();
         Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
+        //args.putX(...) siendo X el tipo de argumento
         fragment.setArguments(args);
         return fragment;
     }
@@ -54,10 +52,12 @@ public class ParadasFragment extends Fragment {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        /*
         if (getArguments() != null) {
             mParam1 = getArguments().getString(ARG_PARAM1);
             mParam2 = getArguments().getString(ARG_PARAM2);
         }
+        */
     }
 
     @Override
