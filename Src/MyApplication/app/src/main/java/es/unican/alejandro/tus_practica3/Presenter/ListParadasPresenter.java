ac56a7754f59231ab3f14766e2edf76f34cd8d22
Javier Sainz-Maza Ruiz <javier.sainzmaza@incentro.com>
2017-10-28 20:00:47 +0200

    Implementación completada: La implementación del listado de paradas ya esta completada

---
+++ Src/MyApplication/app/src/main/java/es/unican/alejandro/tus_practica3/Presenter/ListParadasPresenter.java
@@ -0,0 +1,101 @@
+package es.unican.alejandro.tus_practica3.Presenter;
+
+import android.content.Context;
+import android.os.AsyncTask;
+import android.util.Log;
+import android.widget.Toast;
+
+import java.util.List;
+
+import es.unican.alejandro.tus_practica3.Model.DataLoaders.ParserJSON;
+import es.unican.alejandro.tus_practica3.Model.DataLoaders.RemoteFetch;
+import es.unican.alejandro.tus_practica3.Model.Parada;
+import es.unican.alejandro.tus_practica3.R;
+import es.unican.alejandro.tus_practica3.Views.IListParadasView;
+
+/**
+ * Created by alejandro on 11/10/17.
+ */
+
+public class ListParadasPresenter  {
+    private IListParadasView listParadasView;
+    private List<Parada> listaParadasBus;
+    private RemoteFetch remoteFetchParadas;
+    private Context context;
+
+    public ListParadasPresenter(Context context, IListParadasView listParadasView){
+        this.listParadasView = listParadasView;
+        this.remoteFetchParadas = new RemoteFetch();
+        this.context = context;
+    }// ListParadasPresenter
+
+    public void start(){
+        // Se muestra mensaje de carga
+        listParadasView.showProgress(true);
+        // Se llama a la tarea asíncrona que ejecuta obtenParadas() en un thread en vez de directamente al método
+        new obtenerParadasAsync().execute();
+    }// start
+
+
+    /**
+     * Método a través del cual se almacenan las Paradas de buses en el atributo listaParadasBus
+     * de esta clase. Para realizar esto internamente realiza una llamada a la función
+     * getJSON (RemoteFetch) para seguidamente parsear el JSON obtenido con la llamada
+     * a readArrayParadasBus (ParserJSON)
+     * @return
+     */
+    public boolean obtenParadas(){
+        try {
+            remoteFetchParadas.getJSON(RemoteFetch.URL_PARADAS_BUS);
+            listaParadasBus = ParserJSON.readArrayParadasBus(remoteFetchParadas.getBufferedData());
+            Log.d("ENTRA", "Obten gasolineras:"+listaParadasBus.size());
+            return true;
+        }catch(Exception e){
+            Log.e("ERROR","Error en la obtención de las paradas de Bus: "+e.getMessage());
+            e.printStackTrace();
+            return false;
+        }//try
+    }//obtenParadas
+
+
+    public List<Parada> getListaParadasBus() {
+        return listaParadasBus;
+    }//getListaParadasBus
+
+
+    /**
+     * Método para obtener un cadena de texto con todas las paradas. En esta cadena
+     * se muestra unicamente el nombre de la parada
+     *  @return String con todas las gasolineras separadas por un doble salto de línea
+     */
+    public String getTextoParadas(){
+        String textoParadas="";
+        if(listaParadasBus!=null){
+            for (int i=0; i<listaParadasBus.size(); i++){
+                textoParadas=textoParadas+listaParadasBus.get(i).getNumero()+"\n\n";
+            }//for
+        }else{
+            textoParadas="Sin paradas";
+        }//if
+        return textoParadas;
+    }//getTextoParadas
+
+
+    public class obtenerParadasAsync extends AsyncTask<Object, Boolean, Boolean> {
+
+        @Override
+        protected Boolean doInBackground(Object... objects) {
+            // Se cargan "de fondo" las líneas en el hilo destinado a la tarea
+            return obtenParadas();
+        }
+
+        protected void onPostExecute(Boolean bool) {
+            // Una vez ejecutado, se muestra el listado y el mensaje de carga completa, desactivando el de espera
+            listParadasView.showList(getListaParadasBus());
+            listParadasView.showProgress(false);
+            Toast.makeText(context, R.string.app_carga_datos_ok, Toast.LENGTH_SHORT).show();
+        }
+
+    }
+
+}// ListParadasPresenter
---
+++ Src/MyApplication/app/src/main/java/es/unican/alejandro/tus_practica3/Views/ActivityInicialLogo.java
@@ -0,0 +1,33 @@
+package es.unican.alejandro.tus_practica3.Views;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.support.v7.app.AppCompatActivity;
+
+import es.unican.alejandro.tus_practica3.R;
+
+//Prueba de comentario (Javier) para Git
+
+public class ActivityInicialLogo extends AppCompatActivity {
+
+    // Duración en milisegundos que se mostrará el splash
+    private final int DURACION_SPLASH = 2000; // 2 segundos
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.inicial);
+        getSupportActionBar().hide();
+        new Handler().postDelayed(new Runnable(){
+            public void run(){
+                // Cuando pasen los 3 segundos, pasamos de la actividad principal a paradas
+                Intent intent = new Intent(ActivityInicialLogo.this, MainActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        }, DURACION_SPLASH);
+    }
+}
+
+
