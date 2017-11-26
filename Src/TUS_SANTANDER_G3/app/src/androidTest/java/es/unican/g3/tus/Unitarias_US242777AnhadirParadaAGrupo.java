7beae7d8425fdfeceab4ed5b0c165663a823921d
Javier <jmv52@alumnos.unican.es>
2017-11-26 22:40:06 +0100

    Modificadas pruebas unitarias de "Añadir parada a grupo" (2)
    En este commit se corrige la clase de test asociada a las pruebas unitarias de la correspondiente historia de usuario. Se ha modificado el nombre de la clase de prueba y arreglado algunos errores existentes en los asertos.

--- Src/TUS_SANTANDER_G3/app/src/androidTest/java/es/unican/g3/tus/Unitarias_US242777AnhadirParadaAGrupo.java
+++ Src/TUS_SANTANDER_G3/app/src/androidTest/java/es/unican/g3/tus/Unitarias_US242777AnhadirParadaAGrupo.java
@@ -17,37 +17,27 @@ import static org.junit.Assert.assertTrue;
  * Clase que implementa las pruebas unitarias asociadas a la historia
  * de usuario "Anhadir parada a grupo"
  */
-public class AnhadirParadaAGrupoTest {
+public class Unitarias_US242777AnhadirParadaAGrupo {
 
     @Test
     public void PU1US242777(){
-        List<Parada> paradasList=new ArrayList<>();
         Parada parada1=new Parada(1,"Avenida de Cantabria 10", "213",322);
         Parada parada2=new Parada(2,"Avenida Cantabria 12", "221",323);
         Parada parada3=new Parada(3,"Los Castros 63", "73",45);
-        paradasList.add(parada1);
-        paradasList.add(parada2);
-        paradasList.add(parada3);
 
-        List<Grupo> gruposList=new ArrayList<>();
         Grupo gAguamarina = new Grupo(1,"Aguamarina", "#00C2C2");
         Grupo gAmarillo = new Grupo(2, "Amarillo", "#FFC501");
         Grupo gCian = new Grupo(3, "Cian", "#00A4EB");
-        gruposList.add(gAguamarina);
-        gruposList.add(gAmarillo);
-        gruposList.add(gCian);
 
-        GrupoParada gp1=new GrupoParada(gAguamarina, paradasList.get(0));
-        assertTrue(gp1.getParada().equals(paradasList.get(0)));
-        assertTrue(gp1.getGrupo().equals(paradasList.get(0)));
 
-        GrupoParada gp2=new GrupoParada(gAmarillo, paradasList.get(1));
-        assertTrue(gp2.getParada().equals(paradasList.get(1)));
-        assertTrue(gp2.getGrupo().equals(paradasList.get(1)));
+        GrupoParada gp1=new GrupoParada(gAguamarina, parada1);
+        assertTrue(gp1.getParada().getGrupoAsignado()==gAguamarina.getId());
 
-        GrupoParada gp3=new GrupoParada(gCian, paradasList.get(2));
-        assertTrue(gp3.getParada().equals(paradasList.get(2)));
-        assertTrue(gp3.getGrupo().equals(paradasList.get(2)));
+        GrupoParada gp2=new GrupoParada(gAmarillo, parada2);
+        assertTrue(gp2.getParada().getGrupoAsignado()==gAmarillo.getId());
+
+        GrupoParada gp3=new GrupoParada(gCian, parada3);
+        assertTrue(gp3.getParada().getGrupoAsignado()==gCian.getId());
     }
 
     @Test
@@ -75,10 +65,10 @@ public class AnhadirParadaAGrupoTest {
 
 
         assertTrue((ListGruposPresenter.getGruposConParadas(grupos,gruposParadas)).get(0).equals(gAguamarina));
-        assertTrue((ListGruposPresenter.getGruposConParadas(grupos,gruposParadas)).get(1).equals(parada1));
-        assertTrue((ListGruposPresenter.getGruposConParadas(grupos,gruposParadas)).get(2).equals(parada2));
+        assertTrue((ListGruposPresenter.getGruposConParadas(grupos,gruposParadas)).get(1).equals(gp1.getParada()));
+        assertTrue((ListGruposPresenter.getGruposConParadas(grupos,gruposParadas)).get(2).equals(gp2.getParada()));
         assertTrue((ListGruposPresenter.getGruposConParadas(grupos,gruposParadas)).get(3).equals(gAmarillo));
-        assertTrue((ListGruposPresenter.getGruposConParadas(grupos,gruposParadas)).get(4).equals(parada3));
+        assertTrue((ListGruposPresenter.getGruposConParadas(grupos,gruposParadas)).get(4).equals(gp3.getParada()));
     }
 
 }
